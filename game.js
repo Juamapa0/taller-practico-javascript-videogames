0c4f6ab42129831dfd3529de037371140f254227
juamapa0 <juamapa0@gmail.com>
2022-10-04 15:28:13 -0300

    game

---
+++ game.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+    let canvasSize;
+
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    }else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    
+
+   // window.innerHeight
+   // window.innerWidth
+
+    // game.fillRect(0,0,100,100);
+    // game.clearRect(0,0,100,50);
+
+
+
+    game.font ='25px Verdana'
+    game.fillStyle = 'purle';
+    game.texAling = 'start';
+    game.fillText('Platzi', 25, 25);
+}
